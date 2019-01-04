d897e0788095dc515ff1835f32e55066fdabdc58
Spencer Reyka <spencer.reyka@gmail.com>
2019-01-04 00:56:14 -0500

    added context

---
+++ src/SeachParams.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBox from "./SearchBox";
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="search-route">
+        <SearchBox />
+      </div>
+    );
+  }
+}
+
+export default Search;
